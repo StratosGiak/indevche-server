bf52fb8308c9f2e1e4c5a5df58e986af5499b910
StratosGiak <efstratios.giak@gmail.com>
2024-12-07 04:57:24 +0200

    add type conversions

--- src/database.ts
+++ src/database.ts
@@ -9,7 +9,63 @@ import {
   NewHistory,
   NewRecord,
   Record,
+  Store,
+  User,
 } from "./types.js";
+
+function convertRecord(record: DatabaseRecord) {
+  return {
+    id: record.id,
+    date: record.datek,
+    name: record.onomatep,
+    address: record.odos,
+    area: record.perioxi,
+    city: record.poli,
+    postalCode: record.tk,
+    phoneMobile: record.kinito,
+    phoneHome: record.tilefono,
+    email: record.email,
+    product: record.eidos,
+    manufacturer: record.marka,
+    serial: record.serialnr,
+    hasWarranty: record.warranty,
+    warrantyDate: record.datekwarr,
+    fee: record.pliromi,
+    advance: record.prokatavoli,
+    status: record.katastasi_p,
+    mechanic: record.mastoras_p,
+    photo: record.photo,
+    notesReceived: record.paratiriseis_para,
+    notesRepaired: record.paratiriseis_epi,
+    store: record.katastima,
+    history: record.istoriko.map((h) => convertHistory(h)),
+  } as Record;
+}
+
+function convertUser(user: DatabaseUser) {
+  return {
+    name: user.onoma,
+    username: user.username,
+    password: user.password,
+  } as User;
+}
+
+function convertHistory(history: DatabaseHistory) {
+  return {
+    id: history.id,
+    recordId: history.episkevi_id,
+    mechanic: history.mastoras_p,
+    date: history.datek,
+    notes: history.paratiriseis,
+  } as History;
+}
+
+function convertStore(store: DatabaseStore) {
+  return {
+    area: store.onoma,
+    address: store.odos,
+  } as Store;
+}
 
 const pool = createPool({
   host: process.env.DB_HOST,
@@ -23,7 +79,8 @@ export async function getUser(username: string) {
     "SELECT * FROM mastores WHERE username = ?",
     [username]
   );
-  return result[0];
+  if (!result[0]) return null;
+  return convertUser(result[0]);
 }
 
 export async function getRecord(index: number) {
@@ -32,9 +89,8 @@ export async function getRecord(index: number) {
     [index]
   );
   if (!result[0]) return;
-  const record = result[0];
-  record.istorika = await getAllHistoryOf(record.id);
-  return result[0];
+  result[0].istorika = await getAllHistoryOf(result[0].id);
+  return convertRecord(result[0]);
 }
 
 export async function createRecord(record: NewRecord) {
@@ -123,7 +179,7 @@ export async function getRecordPhoto(index: number) {
     "SELECT photo FROM episkeves WHERE id = ?",
     [index]
   );
-  return result[0];
+  return result[0] as unknown as string | undefined;
 }
 
 export async function addHistory(history: NewHistory) {
@@ -143,7 +199,7 @@ export async function getAllRecords() {
   for (const record of result) {
     record.istorika = await getAllHistoryOf(record.id);
   }
-  return result;
+  return result.map((r) => convertRecord(r));
 }
 
 export async function getAllRecordsByMechanic(id: number) {
@@ -154,7 +210,7 @@ export async function getAllRecordsByMechanic(id: number) {
   for (const record of result) {
     record.istorika = await getAllHistoryOf(record.id);
   }
-  return result;
+  return result.map((r) => convertRecord(r));
 }
 
 export async function getHistory(index: number) {
@@ -162,7 +218,8 @@ export async function getHistory(index: number) {
     "SELECT * FROM istorika WHERE id = ?",
     [index]
   );
-  return result[0];
+  if (!result[0]) return null;
+  return convertHistory(result[0]);
 }
 
 export async function getAllHistoryOf(index: number) {
@@ -172,7 +229,7 @@ export async function getAllHistoryOf(index: number) {
     WHERE i.episkevi_id = ?`,
     [index]
   );
-  return result;
+  return result.map((h) => convertHistory(h));
 }
 
 export async function getAllSuggestions() {
@@ -223,7 +280,7 @@ export async function getStore(id: number) {
     "SELECT onoma, odos FROM katastimata WHERE id = ?",
     [id]
   );
-  return result[0];
+  return convertStore(result[0]);
 }
 
 export async function getRecordDataForSms(id: number) {
@@ -231,5 +288,5 @@ export async function getRecordDataForSms(id: number) {
     "SELECT katastima, mastoras_p, onomatep, kinito FROM episkeves WHERE id = ?",
     [id]
   );
-  return result[0];
+  return convertRecord(result[0]);
 }
